7e7c567433988f51cb702487c77eaec8ad3727a9
rennard <marquezrennard@gmail.com>
2020-03-08 17:29:08 +0800

    setup register feature

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Connect to database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+//Define routes
+app.use("/api/users", require("./routes/api/user"));
+
+app.listen(5000, () => {
+  console.log("server running at port 5000");
+});
